68cc8fd932dec42cba43f538d0ed280886f83263
Kenzie Togami <ket1999@gmail.com>
2017-03-22 20:58:54 -0700

    no more zlib

--- src/main/java/org/rivierarobotics/FrameDecoder.java
+++ src/main/java/org/rivierarobotics/FrameDecoder.java
@@ -1,7 +1,6 @@
 package org.rivierarobotics;
 
 import java.io.InputStream;
-import java.util.zip.InflaterInputStream;
 
 import org.rivierarobotics.protos.Packet.Frame;
 
@@ -13,6 +12,6 @@ public class FrameDecoder {
      * @return the JPEG data stream
      */
     public static InputStream getJpegStreamFromFrame(Frame frame) {
-        return new InflaterInputStream(frame.getJpeg().newInput());
+        return frame.getJpeg().newInput();
     }
 }
